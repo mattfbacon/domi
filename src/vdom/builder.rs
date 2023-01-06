b15b781b90d8d7d31935e1446f61560147d06a3a
Matt Fellenz <matt@felle.nz>
2023-01-05 18:26:36 -0800

    Remove element shorthand methods

--- src/vdom/builder.rs
+++ src/vdom/builder.rs
@@ -128,27 +128,3 @@ impl<'a> DomBuilder<'a> {
 		move || state.build_dom()
 	}
 }
-
-macro_rules! element_methods {
-	($($element:ident),* $(,)?) => {
-		/// Shorter constructors for elements
-		impl DomBuilder<'_> {
-			$(
-				#[inline]
-				pub fn $element(&mut self, id: impl Hash) -> ElementBuilder<'_> {
-					self.element(id, stringify!($element))
-				}
-			)*
-		}
-	}
-}
-
-element_methods![
-	a, abbr, address, area, article, aside, audio, b, base, bdi, bdo, blockquote, br, button, canvas,
-	caption, cite, code, col, colgroup, data, datalist, dd, del, details, dfn, dialog, div, dl, dt,
-	em, embed, fieldset, figcaption, figure, footer, form, h1, h2, h3, h4, h5, h6, header, hgroup,
-	hr, i, iframe, img, input, ins, kbd, label, legent, li, link, main, map, mark, menu, meter, nav,
-	noscript, object, ol, optgroup, option, output, p, picture, pre, progress, q, rp, rt, ruby, s,
-	samp, section, select, slot, small, source, span, strong, sub, summary, sup, table, tbody, td,
-	template, textarea, tfoot, th, thead, time, title, tr, track, u, ul, var, video, wbr,
-];
